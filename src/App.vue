527b18a4012cc6d6cd7af76d78de914d3f4eccc6
IgorMMichels <igor.marcon.michels@gmail.com>
2026-03-03 09:24:06 -0300

    Continuação, falta botao ordenar tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <script setup>
   import { ref } from 'vue'
+
   const tarefas = ref([
-    'Seminário Português',
-    'Seminário Geografia',
-    'Contador Vue',
-    'Seminário Ed. Física'
+  {nome: 'Estudar', status: 'concluida'},
+  {nome: 'Dormir', status: 'concluida'},
+  {nome: 'Comer', status: 'pendente'},
+  {nome: 'Trabalar', status: 'pendente'},
   ])
   const novaTarefa = ref('')
+
+  // Concluir Tarefa
+
 </script>
 
 <template>
   <div class="container">
     <h1>Lista de Tarefas</h1>
     <ul>
-      <li v-for="tarefa in tarefas" :key="tarefa">
-      {{ tarefa }}
+      <li v-for="tarefa in tarefas" :key="tarefa" @click="tarefa.status = tarefa.status === 'concluida' ? 'pendente' : 'concluida'" :class="{concluida: tarefa.status == 'concluida'}">
+      {{ tarefa.nome }}<button @click="tarefas.splice(tarefas.indexOf(tarefa), 1)">X</button>
     </li>
     </ul>
     <input type="text" v-model="novaTarefa">
-    <button @click="tarefas.push(novaTarefa)">Adicionar</button>
+    <button @click="tarefas.push({nome: novaTarefa, status: 'pendente'})">Adicionar</button>
   </div>
 </template>
 
 <style scoped>
+li {
+  cursor: pointer;
+}
+.concluida {
+  color: lime
+}
 </style>
